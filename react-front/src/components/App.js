c569e9744d27028dcd8c82145bac8b9dc1b4a858
Anit <anittankandasamy@gmail.com>
2022-12-01 16:46:04 -0500

    add basic navbar

--- react-front/src/components/App.js
+++ react-front/src/components/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecommendChannels from './RecommendChannels';
 import Home from './Home';
+import Nav from './Nav';
 function App() {
 
   return (
 
     <div className="App">
       <Router>
+        <Nav/>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/recommendchannels" element={<RecommendChannels />} />
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/recommendchannels" element={<RecommendChannels/>} />
         </Routes>
       </Router>
     </div>
